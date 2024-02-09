0d4dbad8a762f3f58b6a863104c72c7f9141d7c0
Layla Alsagheer <lalsaghe@noreply.learn.reboot01.com>
2024-02-09 02:26:18 +0000

    Upload files to ''

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"	
+)
+
+func main() {
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", APIHandler)
+	http.HandleFunc("/details/", DetailsHandler)
+	fmt.Printf("Starting server for testing HTTP on Port 8080 ...\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
