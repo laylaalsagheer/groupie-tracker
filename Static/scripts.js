37ec160ddde7248da60bbd9284720b1e1c27d150
laylaalsagheer <122734644+laylaalsagheer@users.noreply.github.com>
2024-02-11 23:54:19 +0300

    done

--- Static/scripts.js
+++ Static/scripts.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
     $('.burgermenu').on('click', function(){
         $('.mob-nav').toggle("slow");
     });
-}); // Added closing parenthesis here
-
+}); 
 document.querySelectorAll('a.detailsbutton').forEach(function(button) {
     button.addEventListener('click', function(e) {
         e.preventDefault();
+        // Fetch the HTML content
         fetch(e.target.href)
             .then(response => response.text())
             .then(html => {
+                // Update the content of the .container element
                 document.querySelector('.container').innerHTML = html;
+                // Update the browser's URL without reloading the page
+                history.pushState({}, '', e.target.href);
             });
     });
-}); // Added closing parenthesis here
+});
