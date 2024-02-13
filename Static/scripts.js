5f4ae7dff476c6769aaddbab07cc61ab5fe8a3d4
laylaalsagheer <122734644+laylaalsagheer@users.noreply.github.com>
2024-02-13 22:48:22 +0300

    project is ready

--- Static/scripts.js
+++ Static/scripts.js
@@ -4,17 +4,17 @@ $(document).ready(function(){
         $('.mob-nav').toggle("slow");
     });
 }); 
-document.querySelectorAll('a.detailsbutton').forEach(function(button) {
-    button.addEventListener('click', function(e) {
-        e.preventDefault();
-        // Fetch the HTML content
-        fetch(e.target.href)
-            .then(response => response.text())
-            .then(html => {
-                // Update the content of the .container element
-                document.querySelector('.container').innerHTML = html;
-                // Update the browser's URL without reloading the page
-                history.pushState({}, '', e.target.href);
-            });
-    });
-});
+// document.querySelectorAll('a.detailsbutton').forEach(function(button) {
+//     button.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         // Fetch the HTML content
+//         fetch(e.target.href)
+//             .then(response => response.text())
+//             .then(html => {
+//                 // Update the content of the .container element
+//                 document.querySelector('.container').innerHTML = html;
+//                 // Update the browser's URL without reloading the page
+//                 history.pushState({}, '', e.target.href);
+//             });
+//     });
+// });
--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", APIHandler)
 	http.HandleFunc("/details/", DetailsHandler)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	fmt.Printf("Starting server for testing HTTP on Port 8080 ...\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
